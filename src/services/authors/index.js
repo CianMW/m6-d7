13ee95be8c4eb92c171ac357b68f70ac1e86a24b
CianMW <markwick.cian@gmail.com>
2021-11-11 13:16:25 +0000

    Delete author

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,52 +1,78 @@
+import express from "express";
+import createHttpError from "http-errors";
+import q2m from "query-to-mongo";
+import AuthorModel from "./authorSchema.js";
 
-import express from "express"
-import createHttpError from "http-errors"
-import q2m from "query-to-mongo"
-import AuthorModel from "./authorSchema.js"
-
-const authorsRouter = express.Router()
-
+const authorsRouter = express.Router();
 
 //gets all the authors
 authorsRouter.get("/", async (req, res, next) => {
-    try{
-        const mongoQuery = q2m(req.query)
-        const { total, authors } = await AuthorModel.findAuthors(mongoQuery)
-        res.send({ links: mongoQuery.links("/authors", total), pageTotal: Math.ceil(total / mongoQuery.options.limit), total, authors })
-    }
-    catch(error) {
-        next(error)
-    }
-})
-
+  try {
+    const mongoQuery = q2m(req.query);
+    const { total, authors } = await AuthorModel.findAuthors(mongoQuery);
+    res.send({
+      links: mongoQuery.links("/authors", total),
+      pageTotal: Math.ceil(total / mongoQuery.options.limit),
+      total,
+      authors,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 //posts a new author/user
 authorsRouter.post("/", async (req, res, next) => {
-    try{
-        const newAuthor = new AuthorModel(req.body) 
+  try {
+    const newAuthor = new AuthorModel(req.body);
 
-        const { _id } = await newAuthor.save() 
-        // .save() === writeToFile
-        res.status(201).send({ _id })
-    }
-    catch(error){
-        next(error)
-    }
-})
+    const { _id } = await newAuthor.save();
+    // .save() === writeToFile
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
 
 //get by id
-authorsRouter.get("/", async (req, res, next) => {})
+authorsRouter.get("/:authorId", async (req, res, next) => {
+    try {
+        const id = req.params.authorId
+    
+        const author = await AuthorModel.findById(id)
+        if (author) {
+          res.send(author)
+        } else {
+          next(createHttpError(404, `Author with the ${id} not found!`))
+        }
+      } catch (error) {
+        next(error)
+      }
+    })
 
 //delete specific user
-authorsRouter.delete("/", async (req, res, next) => {})
+authorsRouter.delete("/:authorId", async (req, res, next) => {
+  try {
+    const id = req.params.authorId
+    const deletedAuthor = await AuthorModel.deleteOne({id: id})
+
+    if(deletedAuthor){
+        res.status(204).send(`Author with id ${id} has been deleted`)
+    }else {
+        res.send(`Author with id ${id} not found`)
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.put("/:authorId", async (req, res, next) => {
+    try {
 
 
-authorsRouter.put("/", async (req, res, next) => {})
+    } catch (error) {
+      next(error);
+    }
+});
 
-
-
-
-
-
-
-export default authorsRouter
+export default authorsRouter;
